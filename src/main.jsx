443ba9ebe65cbd1b42e9dd46bf6420c69b8965ee
sebaparaguay <sebaparaguay@gmail.com>
2026-05-17 00:10:05 -0300

    Add PostHog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import posthog from 'posthog-js'
+
+posthog.init(
+  'phc_DnYkPwUVzgtgXzbyDfQzUqapHPW3CjwKDjgo5ZGNwcm7',
+  {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only'
+  }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
